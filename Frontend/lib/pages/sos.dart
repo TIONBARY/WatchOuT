3516fb94ff511dc92e4593042ae1447e9e3eaba2
김시언 <rlatldjs123@gmail.com>
2022-10-24 13:51:15 +0900

    :sparkles: sos.dart 추가

---
+++ Frontend/lib/pages/sos.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class sos extends StatefulWidget {
+  const sos({Key? key}) : super(key: key);
+
+  @override
+  State<sos> createState() => _sosState();
+}
+
+class _sosState extends State<sos> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
