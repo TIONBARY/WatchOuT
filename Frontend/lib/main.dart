e7824baafd97f334b46065fb5e1090db0c0ec2fb
윤지환 <dddalang4@naver.com>
2022-10-24 17:53:16 +0900

    :sparkles: 현재 위치 정보를 얻기 위한 권한 허용 여부 추가

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:homealone/googleLogin/loading_page.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'googleLogin/root_page.dart';
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    _permission();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'WatchOut',
@@ -66,4 +68,47 @@ class HomePage extends StatelessWidget {
       },
     );
   }
+}
+
+void _permission() async {
+  var requestStatus = await Permission.location.request();
+  // var requestStatus = await Permission.locationAlways.request();
+
+  if (await Permission.contacts.request().isGranted) {
+    // Either the permission was already granted before or the user just granted it.
+  }
+
+// You can request multiple permissions at once.
+  Map<Permission, PermissionStatus> statuses = await [
+    Permission.locationAlways,
+    Permission.manageExternalStorage,
+    Permission.storage,
+  ].request();
+  print(statuses[Permission.location]);
+
+  var status = await Permission.location.status;
+  if (requestStatus.isGranted && status.isLimited) {
+    // isLimited - 제한적 동의 (ios 14 < )
+    // 요청 동의됨
+    print("isGranted");
+    if (await Permission.locationWhenInUse.serviceStatus.isEnabled) {
+      // 요청 동의 + gps 켜짐
+    } else {
+      // 요청 동의 + gps 꺼짐
+      print("serviceStatusIsDisabled");
+    }
+  } else if (requestStatus.isPermanentlyDenied || status.isPermanentlyDenied) {
+    // 권한 요청 거부, 해당 권한에 대한 요청에 대해 다시 묻지 않음 선택하여 설정화면에서 변경해야함. android
+    print("isPermanentlyDenied");
+    openAppSettings();
+  } else if (status.isRestricted) {
+    // 권한 요청 거부, 해당 권한에 대한 요청을 표시하지 않도록 선택하여 설정화면에서 변경해야함. ios
+    print("isRestricted");
+    openAppSettings();
+  } else if (status.isDenied) {
+    // 권한 요청 거절
+    print("isDenied");
+  }
+  print("requestStatus ${requestStatus.name}");
+  print("status ${status.name}");
 }
