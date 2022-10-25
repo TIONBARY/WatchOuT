54ed04cc7da414f5ee99c1d9a9db19ee8dcb3127
한준수 <junsoohan94@gmail.com>
2022-10-25 17:53:13 +0900

    :bug: Wear OS 데이터 전송 버그 해결
    {"HEART_RATE": 값(Double.toString())}의 형식으로 양방향 데이터 교환 가능

--- WearOS/Wearable/src/main/java/com/ssafy/homealone/MainActivity.kt
+++ WearOS/Wearable/src/main/java/com/ssafy/homealone/MainActivity.kt
@@ -17,17 +17,26 @@
 package com.ssafy.homealone
 
 import android.Manifest
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.wearable.CapabilityClient
+import com.google.android.gms.wearable.Node
+import com.google.android.gms.wearable.PutDataRequest
+import com.google.android.gms.wearable.Wearable
 import com.ssafy.homealone.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
 
 /**
  * Activity displaying the app UI. Notably, this binds data from [MainViewModel] to views on screen,
@@ -36,14 +45,33 @@ import kotlinx.coroutines.flow.collect
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val dataClient by lazy { Wearable.getDataClient(this) }
+    private val messageClient by lazy { Wearable.getMessageClient(this) }
+    private val capabilityClient by lazy { Wearable.getCapabilityClient(this) }
+    private val nodeClient by lazy { Wearable.getNodeClient(this) }
+    private val channelName = "watch_connectivity"
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var permissionLauncher: ActivityResultLauncher<String>
 
     private val viewModel: MainViewModel by viewModels()
 
+    private val clientDataViewModel by viewModels<ClientDataViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+//        데이터 전송 관련
+//        setContent {
+//            MainApp(
+//                events = clientDataViewModel.events,
+//                image = clientDataViewModel.image,
+//                onQueryOtherDevicesClicked = ::onQueryOtherDevicesClicked,
+//                onQueryMobileCameraClicked = ::onQueryMobileClicked
+//            )
+//        }
+
+//       심박수 정보 관련
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -104,5 +132,147 @@ class MainActivity : AppCompatActivity() {
             binding.lastMeasuredValue.isVisible = it
             binding.heart.isVisible = it
         }
+    }
+
+//    테스트용 전송버튼
+    fun onClickInMain(view: View) {
+        when(binding.lastMeasuredValue.text){
+            binding.lastMeasuredValue.text -> {
+
+//                val intent = Intent(this, DataMainActivity::class.java)
+//                intent.putExtra("heartRate",  binding.lastMeasuredValue.text)
+                Log.d("심박수", binding.lastMeasuredValue.text.toString())
+//                it.id, channelName, messageData
+                sendHeartRateData(binding.lastMeasuredValue.text)
+//                sendMessage(binding.lastMeasuredValue.text.toString())
+//                startActivity(intent)
+            }
+        }
+    }
+
+    //데이터 업데이트
+    private fun sendHeartRateData(data: Any) {
+        val eventData = objectToBytes(mutableMapOf(Pair("HEART_RATE", data.toString().trim())))
+        val dataItem = PutDataRequest.create("/$channelName")
+        dataItem.data = eventData
+        dataClient.putDataItem(dataItem)
+            .addOnSuccessListener { Log.d("데이터 전송 성공", data.toString()) }
+            .addOnFailureListener {  Log.e("데이터 전송 실패", it.message.toString()) }
+
+    }
+
+    // 메세지 전송
+    private fun sendMessage(message: String) {
+        Log.d("메세지", message)
+        val messageData = objectToBytes(mutableMapOf(Pair("HEART_RATE", message)))
+
+        Log.d("메세지 전송", objectFromBytes(messageData).toString())
+        nodeClient.connectedNodes.addOnSuccessListener { nodes ->
+            nodes.forEach {
+                messageClient.sendMessage(it.id, channelName, messageData)
+
+//                messageClient.sendMessage(it.id, "/$channelName", messageData)
+            }
+        }
+    }
+
+    private fun objectToBytes(`object`: Any): ByteArray {
+        val baos = ByteArrayOutputStream()
+        val oos = ObjectOutputStream(baos)
+        oos.writeObject(`object`)
+        return baos.toByteArray()
+    }
+
+    private fun objectFromBytes(bytes: ByteArray): Any {
+        val bis = ByteArrayInputStream(bytes)
+        val ois = ObjectInputStream(bis)
+        return ois.readObject()
+    }
+
+//    private fun onQueryOtherDevicesClicked() {
+//        lifecycleScope.launch {
+//            try {
+//                val nodes = getCapabilitiesForReachableNodes()
+//                    .filterValues { "mobile" in it || "wear" in it }.keys
+//                displayNodes(nodes)
+//            } catch (cancellationException: CancellationException) {
+//                throw cancellationException
+//            } catch (exception: Exception) {
+//                Log.d(TAG, "Querying nodes failed: $exception")
+//            }
+//        }
+//    }
+//
+//    private fun onQueryMobileClicked() {
+//        lifecycleScope.launch {
+//            try {
+//                val nodes = getCapabilitiesForReachableNodes()
+//                    .filterValues { "mobile" in it }.keys
+//                displayNodes(nodes)
+//            } catch (cancellationException: CancellationException) {
+//                throw cancellationException
+//            } catch (exception: Exception) {
+//                Log.d(TAG, "Querying nodes failed: $exception")
+//            }
+//        }
+//    }
+
+    /**
+     * Collects the capabilities for all nodes that are reachable using the [CapabilityClient].
+     *
+     * [CapabilityClient.getAllCapabilities] returns this information as a [Map] from capabilities
+     * to nodes, while this function inverts the map so we have a map of [Node]s to capabilities.
+     *
+     * This form is easier to work with when trying to operate upon all [Node]s.
+     */
+//    private suspend fun getCapabilitiesForReachableNodes(): Map<Node, Set<String>> =
+//        capabilityClient.getAllCapabilities(CapabilityClient.FILTER_REACHABLE)
+//            .await()
+//            // Pair the list of all reachable nodes with their capabilities
+//            .flatMap { (capability, capabilityInfo) ->
+//                capabilityInfo.nodes.map { it to capability }
+//            }
+//            // Group the pairs by the nodes
+//            .groupBy(
+//                keySelector = { it.first },
+//                valueTransform = { it.second }
+//            )
+//            // Transform the capability list for each node into a set
+//            .mapValues { it.value.toSet() }
+//
+//    private fun displayNodes(nodes: Set<Node>) {
+//        val message = if (nodes.isEmpty()) {
+//            getString(R.string.no_device)
+//        } else {
+//            getString(R.string.connected_nodes, nodes.joinToString(", ") { it.displayName })
+//        }
+//
+//        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+//    }
+
+    override fun onResume() {
+        super.onResume()
+        dataClient.addListener(clientDataViewModel)
+        messageClient.addListener(clientDataViewModel)
+        capabilityClient.addListener(
+            clientDataViewModel,
+            Uri.parse("wear://"),
+            CapabilityClient.FILTER_REACHABLE
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        dataClient.removeListener(clientDataViewModel)
+        messageClient.removeListener(clientDataViewModel)
+        capabilityClient.removeListener(clientDataViewModel)
+    }
+
+    companion object {
+        private const val TAG = "DataMainActivity"
+
+        private const val CAMERA_CAPABILITY = "camera"
+        private const val WEAR_CAPABILITY = "wear"
+        private const val MOBILE_CAPABILITY = "mobile"
     }
 }
