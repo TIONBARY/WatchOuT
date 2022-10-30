ef661a84688f806cda5dde0d583bdc47a91943da
최윤정 <chyuje96@naver.com>
2022-10-30 23:33:46 +0900

    :wrench: 회원가입시 도로명주소로 입력하도록 변경

--- Frontend/lib/components/utils/double_click_pop.dart
+++ Frontend/lib/components/utils/double_click_pop.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:sizer/sizer.dart';
 
 DateTime? currentBackPressTime;
 
