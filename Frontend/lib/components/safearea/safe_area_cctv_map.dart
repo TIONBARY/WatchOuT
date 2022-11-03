bcdb3e3c29b590afc7979e0a9fc1ddc82bd5cf9f
윤지환 <dddalang4@naver.com>
2022-11-03 16:36:52 +0900

    :lipstick: CCTV 마커 이미지 변경

--- Frontend/lib/components/safearea/safe_area_cctv_map.dart
+++ Frontend/lib/components/safearea/safe_area_cctv_map.dart
@@ -357,7 +357,7 @@ class _SafeAreaCCTVMapState extends State<SafeAreaCCTVMap> {
     var markers = [];
 
     function addMarker(position) {
-      var imageSrc = 'https://firebasestorage.googleapis.com/v0/b/homealone-6ef54.appspot.com/o/cctvMarker.png?alt=media&token=1ddb3640-c595-4dd8-813c-f1e8ef1df6e0', // 마커이미지의 주소입니다    
+      var imageSrc = 'https://firebasestorage.googleapis.com/v0/b/homealone-6ef54.appspot.com/o/cctv.png?alt=media&token=42b7c032-e455-456c-993e-a29fdafb9e17', // 마커이미지의 주소입니다    
           imageSize = new kakao.maps.Size(40, 40); // 마커이미지의 크기입니다
           // imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
