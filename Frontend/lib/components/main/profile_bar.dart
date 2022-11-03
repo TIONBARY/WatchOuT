feda86303c35af6a1841800fb1051164a965d363
chyuje96 <74063824+chyuje96@users.noreply.github.com>
2022-11-03 17:59:04 +0900

    :sparkles: 프로필 이미지 변경 수정중

--- Frontend/lib/components/main/profile_bar.dart
+++ Frontend/lib/components/main/profile_bar.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:homealone/components/singleton/is_check.dart';
+import 'package:homealone/constants.dart';
+import 'package:homealone/main.dart';
 import 'package:homealone/providers/user_provider.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 
@@ -14,11 +17,23 @@ class ProfileBar extends StatefulWidget {
 }
 
 class _ProfileBarState extends State<ProfileBar> {
+  final picker = ImagePicker();
+  File? profileImage;
+
+  Future<void> chooseImage() async {
+    var choosedimage = await picker.pickImage(source: ImageSource.gallery);
+    //set source: ImageSource.camera to get image from camera
+    setState(() {
+      profileImage = File(choosedimage!.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.fromLTRB(4.w, 3.h, 4.w, 1.h),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Flexible(
             flex: 5,
@@ -30,9 +45,19 @@ class _ProfileBarState extends State<ProfileBar> {
                   backgroundImage: NetworkImage(
                       Provider.of<MyUserInfo>(context, listen: false)
                           .profileImage),
-                  // child: GestureDetector(
-                  //   onTap: () => doCheck(context),
-                  // ),
+                  child: GestureDetector(
+                    onTap: () => showDialog(
+                      context: context,
+                      builder: (context) {
+                        return ProfileImageDialog(
+                          profileImage,
+                          onpresseds: () => {
+                            chooseImage(),
+                          },
+                        );
+                      },
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -48,6 +73,74 @@ class _ProfileBarState extends State<ProfileBar> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ProfileImageDialog extends StatelessWidget {
+  const ProfileImageDialog(
+    this.profileImage, {
+    Key? key,
+    required this.onpresseds,
+  }) : super(key: key);
+
+  final onpresseds;
+  final profileImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.5),
+      ),
+      child: Container(
+        padding: EdgeInsets.fromLTRB(5.w, 2.5.h, 5.w, 1.25.h),
+        height: 25.h,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Title(
+              color: nColor,
+              child: Text(
+                "프로필 이미지 변경",
+                style: TextStyle(
+                  color: nColor,
+                  fontSize: 15.sp,
+                ),
+              ),
+            ),
+            Container(
+              height: 75,
+              width: 75,
+              child: profileImage == null
+                  ? CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          Provider.of<MyUserInfo>(context, listen: false)
+                              .profileImage),
+                    )
+                  : new CircleAvatar(
+                      backgroundImage: new FileImage(
+                        File(profileImage!.path),
+                      ),
+                    ),
+            ),
+            Container(
+              child: ElevatedButton.icon(
+                onPressed: onpresseds,
+                icon: Icon(Icons.image),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: nColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    )),
+                label: Text(
+                  "프로필 이미지 선택",
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
