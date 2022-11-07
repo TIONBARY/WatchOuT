fe509c1dbfecffb5fa8871138337a8f21d945ec5
윤지환 <dddalang4@naver.com>
2022-11-07 10:52:28 +0900

    :sparkles: MMS 메세지 API 추가
    * 신고 기능을 위한 이미지 첨부가 가능한 MMS 메세지 API 추가

--- MessageAPI/src/main/java/com/ssafy/watchout/api/service/MessageServiceImpl.java
+++ MessageAPI/src/main/java/com/ssafy/watchout/api/service/MessageServiceImpl.java
@@ -1,20 +1,25 @@
 package com.ssafy.watchout.api.service;
 
+import java.io.File;
 import java.io.IOError;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.google.gson.Gson;
+import com.ssafy.watchout.api.domain.dto.MMSMessageRequestDto;
 import com.ssafy.watchout.api.domain.dto.MessageRequestDto;
 import com.ssafy.watchout.api.domain.dto.TokenDto;
 
 import okhttp3.MultipartBody;
+import okhttp3.MultipartBody.Builder;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -25,22 +30,26 @@ public class MessageServiceImpl implements MessageService {
 
 	private static final String SMS_OAUTH_TOKEN_URL = "https://sms.gabia.com/oauth/token"; // ACCESS TOKEN 발급 API URL
 																							// 입니다.
-	private static final String SMS_SEND_URL = "https://sms.gabia.com/api/send/lms"; // SMS 발송 API URL입니다.
-	
-	private static final String sender = "01090319389";
-	
+	private static final String LMS_SEND_URL = "https://sms.gabia.com/api/send/lms"; // SMS 발송 API URL입니다.
+
+	private static final String MMS_SEND_URL = "https://sms.gabia.com/api/send/mms";
+
+	private static final String SENDER = "01090319389";
+
+	private static final String REF_KEY = "WATCH_OUT";
+
+	private static final String SUBJECT = "Watch Out 메세지";
+
 	@Value("${open-api.sms-id}")
 	String smsId;
-	
+
 	@Value("${open-api.api-key}")
 	String apiKey;
-	
+
 	@Override
 	public TokenDto authorize() throws IOException {
 		String authValue = Base64.getEncoder()
-				.encodeToString(String.format("%s:%s", smsId, apiKey).getBytes(StandardCharsets.UTF_8)); // Authorization
-																											// // 입력할
-																											// 값입니다.
+				.encodeToString(String.format("%s:%s", smsId, apiKey).getBytes(StandardCharsets.UTF_8));
 
 		// 사용자 인증 API 를 호출합니다.
 		OkHttpClient client = new OkHttpClient();
@@ -71,23 +80,23 @@ public class MessageServiceImpl implements MessageService {
 
 		// SMS 발송 API 를 호출합니다.
 		OkHttpClient client = new OkHttpClient();
-		
+
 		StringBuilder receiversToStringBuilder = new StringBuilder();
 		for (String receiver : requestDto.getReceivers()) {
 			receiversToStringBuilder.append(receiver).append(",");
 		}
-		receiversToStringBuilder.setLength(receiversToStringBuilder.length()-1);
+		receiversToStringBuilder.setLength(receiversToStringBuilder.length() - 1);
 		String receiversToString = receiversToStringBuilder.toString();
 		RequestBody requestBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
 				.addFormDataPart("phone", receiversToString) // 수신번호를 입력해 주세요. (수신번호가 두 개 이상인 경우 ',' 를 이용하여입력합니다.
-																	// ex) 01011112222,01033334444)
-				.addFormDataPart("callback", sender) // 발신번호를 입력해 주세요.
+																// ex) 01011112222,01033334444)
+				.addFormDataPart("callback", SENDER) // 발신번호를 입력해 주세요.
 				.addFormDataPart("message", requestDto.getMessage()) // SMS 내용을 입력해 주세요.
-				.addFormDataPart("refkey", "WATCH_OUT") // 발송 결과 조회를 위한 임의의 랜덤 키 값을 입력해 주세요.
-				.addFormDataPart("subject", "Watch Out 메세지")
-				.build();
+				.addFormDataPart("refkey", REF_KEY + LocalDateTime.now().toString()) // 발송 결과 조회를 위한 임의의 랜덤 키 값을 입력해
+																						// 주세요.
+				.addFormDataPart("subject", SUBJECT).build();
 
-		Request request = new Request.Builder().url(SMS_SEND_URL).post(requestBody)
+		Request request = new Request.Builder().url(LMS_SEND_URL).post(requestBody)
 				.addHeader("Content-Type", "application/x-www-form-urlencoded")
 				.addHeader("Authorization", "Basic " + authValue).addHeader("cache-control", "no-cache").build();
 
@@ -105,4 +114,42 @@ public class MessageServiceImpl implements MessageService {
 
 	}
 
+	@Override
+	public void sendMMSMessage(String accessToken, MMSMessageRequestDto requestDto) throws IOException {
+		String authValue = Base64.getEncoder()
+				.encodeToString(String.format("%s:%s", smsId, accessToken).getBytes(StandardCharsets.UTF_8));
+
+		// MMS 발송 API 를 호출합니다.
+		OkHttpClient client = new OkHttpClient();
+		StringBuilder receiversToStringBuilder = new StringBuilder();
+		for (String receiver : requestDto.getReceivers()) {
+			receiversToStringBuilder.append(receiver).append(",");
+		}
+		receiversToStringBuilder.setLength(receiversToStringBuilder.length() - 1);
+		String receiversToString = receiversToStringBuilder.toString();
+		Builder requestBodyBuilder = new MultipartBody.Builder().setType(MultipartBody.FORM)
+				.addFormDataPart("phone", receiversToString).addFormDataPart("callback", SENDER) // 발신번호를 입력해 주세요.
+				.addFormDataPart("message", requestDto.getMessage()) // MMS 내용을 입력해 주세요.
+				.addFormDataPart("refkey", REF_KEY + LocalDateTime.now().toString()) // 발송 결과 조회를 위한 임의의 랜덤 키를 입력해 주세요.
+				.addFormDataPart("subject", SUBJECT) // MMS 제목을 입력해 주세요.
+				.addFormDataPart("image_cnt", requestDto.getFiles().size()+"") // 이미지 첨부 개수를 입력해 주세요. (최대 3개까지 첨부 가능합니다.)
+				;
+		for (int i = 0; i < requestDto.getFiles().size(); i++) {
+			MultipartFile file = requestDto.getFiles().get(i);
+			requestBodyBuilder.addFormDataPart("images"+i, file.getName(), RequestBody.create(new File(file.getOriginalFilename()),
+					MultipartBody.FORM));
+		}
+		RequestBody requestBody = requestBodyBuilder.build();
+		Request request = new Request.Builder().url(MMS_SEND_URL).post(requestBody)
+				.addHeader("Authorization", "Basic " + authValue).build();
+
+		Response response = client.newCall(request).execute();
+
+		// Response 를 key, value 로 확인하실 수 있습니다.
+		HashMap<String, String> result = new Gson().fromJson(Objects.requireNonNull(response.body()).string(),
+				HashMap.class);
+		for (String key : result.keySet()) {
+			System.out.printf("%s: %s%n", key, result.get(key));
+		}
+	}
 }
