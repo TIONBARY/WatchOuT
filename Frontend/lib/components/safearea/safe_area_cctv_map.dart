a86410acda92e1902ed0a398e6916fe44d3e4409
김시언 <rlatldjs123@gmail.com>
2022-11-10 17:28:57 +0900

    :zap: 불필요한 코드 삭제

--- Frontend/lib/components/safearea/safe_area_cctv_map.dart
+++ Frontend/lib/components/safearea/safe_area_cctv_map.dart
@@ -5,7 +5,6 @@ import 'dart:math' show Random, asin, cos, sqrt;
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
