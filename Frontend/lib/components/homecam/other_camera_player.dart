08c5c766c388f85f4ae24bd31f2c883bf958d770
윤지환 <dddalang4@naver.com>
2022-11-16 16:51:51 +0900

    :sparkles: 홈캠 공유 시 만료 시간이 지날 경우 홈캠 자동 종료되도록 변경
    * 홈캠 공유 시작으로부터 30분 경과 시 더 이상 홈캠 화면을 볼 수 없고 홈캠이 만료되었다는 Dialog 표시

--- Frontend/lib/components/homecam/other_camera_player.dart
+++ Frontend/lib/components/homecam/other_camera_player.dart
@@ -1,11 +1,19 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_vlc_player/flutter_vlc_player.dart';
+import 'package:homealone/components/dialog/basic_dialog.dart';
+import 'package:sizer/sizer.dart';
 
 class OtherCameraPlayer extends StatefulWidget {
-  const OtherCameraPlayer({Key? key, required this.url}) : super(key: key);
+  const OtherCameraPlayer(
+      {Key? key, required this.url, required this.expiredTime})
+      : super(key: key);
 
-  final url;
+  final Timestamp expiredTime;
+  final String url;
 
   @override
   _OtherCameraPlayerState createState() => _OtherCameraPlayerState();
@@ -13,19 +21,40 @@ class OtherCameraPlayer extends StatefulWidget {
 
 class _OtherCameraPlayerState extends State<OtherCameraPlayer> {
   late VlcPlayerController playerController;
-
+  late Timer timer;
   @override
   void initState() {
+    super.initState();
     // 가로 화면 고정
     SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
     playerController = VlcPlayerController.network(widget.url, autoPlay: true);
-    super.initState();
+    Future.delayed(Duration.zero, () {
+      checkExpired();
+    });
+    timer = Timer.periodic(Duration(minutes: 1), (timer) {
+      checkExpired();
+    });
+  }
+
+  void checkExpired() {
+    if (DateTime.parse(widget.expiredTime.toDate().toString())
+        .isBefore(DateTime.now())) {
+      playerController.dispose();
+      showDialog(
+          context: context,
+          builder: (_) => BasicDialog(
+              EdgeInsets.fromLTRB(5.w, 2.5.h, 5.w, 0.5.h),
+              12.5.h,
+              "홈캠이 만료되었습니다.",
+              null)).then((response) => Navigator.of(context).pop());
+    }
   }
 
   @override
   void dispose() {
     // 세로 화면 고정
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    timer.cancel();
     super.dispose();
   }
 
